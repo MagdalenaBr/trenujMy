9ed59dd387b32e3e12da4bfd6546a0d8bd99a55a
Magdalena <magdalena.a.broniszewska@gmail.com>
2024-04-04 17:44:14 +0200

    schedule width change

--- src/ui/MainContainer.tsx
+++ src/ui/MainContainer.tsx
@@ -1,19 +1,23 @@
 type PropsType = {
-	title: string;
-	children: React.ReactNode;
-	buttons?: React.ReactNode;
+  title: string;
+  children: React.ReactNode;
+  buttons?: React.ReactNode;
 };
 
 function MainContainer({ children, title, buttons }: PropsType) {
-	return (
-		<div className='w-[80%] mx-auto flex flex-col relative'>
-			<div className={buttons? 'flex justify-between' : ''}>
-				<h2 className='text-[2rem] text-start mb-5 text-primaryTextColor'>{title}</h2>
-				{buttons}
-			</div>
-			{children}
-		</div>
-	);
+  return (
+    <div
+      className={`${title === "Grafik" ? "w-[90%]" : "w-[80%]"} relative mx-auto flex flex-col`}
+    >
+      <div className={buttons ? "flex justify-between" : ""}>
+        <h2 className="mb-5 text-start text-[2rem] text-primaryTextColor">
+          {title}
+        </h2>
+        {buttons}
+      </div>
+      {children}
+    </div>
+  );
 }
 
 export default MainContainer;
