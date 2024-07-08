596c6caeff8a6c2239f975707c5c80487b85ce8d
Magdalena <magdalena.a.broniszewska@gmail.com>
2024-07-08 18:22:22 +0200

    nav-style

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -28,7 +28,7 @@ function AppLayout() {
         }
       >
         <button
-          className="absolute right-1 top-1 z-20 text-2xl lg:hidden"
+          className="absolute right-1 top-1 z-20 text-3xl lg:hidden"
           onClick={showNav}
         >
           {navigationVisibility ? <HiMenuAlt3 /> : <HiOutlineXMark />}
@@ -36,7 +36,7 @@ function AppLayout() {
       </div>
 
       <div
-        className={`${navigationVisibility === "hidden" ? "fixed translate-x-[-100%]" : " absolute bottom-0 left-0 right-0 top-0 z-10 overflow-hidden  bg-slate-900"}  font-semibold lg:relative lg:translate-x-0`}
+        className={`${navigationVisibility === "hidden" ? "fixed translate-x-[-100%]" : " absolute bottom-0 left-0 right-0 top-auto z-10 overflow-hidden  bg-slate-900"}  font-semibold lg:relative lg:translate-x-0`}
       >
         <Logo />
         <Sidebar setNavigationVisibility={setNavigationVisibility} />
