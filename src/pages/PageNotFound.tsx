e3ea3228a69a395f4e2b61929118272f4863817e
Magdalena <magdalena.a.broniszewska@gmail.com>
2024-07-10 10:24:09 +0200

    file rename

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,13 @@
+import MainContainer from "../ui/MainContainer";
+
 function PageNotFound() {
-  return <div className="">nie ma takiej strony</div>;
+  return (
+    <div className="w-screen">
+      <h1 className="translate-y-56 text-center text-3xl font-bold text-slate-300">
+        Nie znaleziono strony.
+      </h1>
+    </div>
+  );
 }
 
 export default PageNotFound;
