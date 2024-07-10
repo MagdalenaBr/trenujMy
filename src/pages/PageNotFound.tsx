1c1f4702731f01d5a834018e6feed79ca9d8d05a
Magdalena <magdalena.a.broniszewska@gmail.com>
2024-07-10 11:23:38 +0200

    PageNotFound style

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,10 +1,20 @@
+import { HiArrowUturnLeft } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
+  const navigate = useNavigate();
   return (
-    <div className="w-screen">
-      <h1 className="uppercase translate-y-56 text-center text-3xl font-bold text-slate-300">
-        Nie znaleziono strony.
-      </h1>
+    <div className="flex w-screen translate-y-56 flex-col items-center gap-10 text-slate-300">
+      <h1 className="text-3xl font-bold">Nie znaleziono strony.</h1>
+      <button
+        onClick={() => navigate('/')}
+        className="flex items-center gap-3 border-2 border-accentColor2 px-2 py-2 font-semibold uppercase tracking-wider"
+      >
+        <span className="text-2xl">
+          <HiArrowUturnLeft />
+        </span>
+        Strona główna
+      </button>
     </div>
   );
 }
